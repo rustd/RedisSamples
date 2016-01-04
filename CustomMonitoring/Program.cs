6137e8c05b8cf898dc84924ad5b4b0457d530dd4
Pranav Rastogi <raspranav@gmail.com>
2016-01-04 12:14:51 -0800

    Adding sample for China

--- CustomMonitoring/Program.cs
+++ CustomMonitoring/Program.cs
@@ -15,36 +15,28 @@ namespace CustomMonitoring
 {
     class Program
     {
-        public static string subscriptionId = ""; //Get this from the Azure Portal
-        public static string resourceGroupName = "";//Get this from the Azure Portal
+        public static string subscriptionId = ""; // Get this from the Azure Portal
+        public static string resourceGroupName = ""; //,Get this from the Azure Portal
         public static string cacheName = ""; // This is just the name (without redis.cache.windows.net)
-       
+
         // Use the following link to learn how to setup this app to access Active Directory
         // https://msdn.microsoft.com/en-us/library/azure/dn790557.aspx#bk_portal
         public static string tenantId = "";
         public static string clientId = "";
-        public static string redirectUri = "";
+        public static string redirectUri = ""; // Your app name
 
         static void Main(string[] args)
         {
             // Define the base URI for management operations
-            Uri baseUri = new Uri("https://management.azure.com");
+            var azureUrl = "https://management.azure.com";  // or "https://management.chinacloudapi.cn/" for China
+            Uri baseUri = new Uri(azureUrl);
 
             string token = GetAuthorizationHeader();
             var startTime = DateTime.Now;
             var endTime = startTime.ToUniversalTime().AddHours(1.0).ToLocalTime();
             var redisConnection = "";
-            
-            ConnectionMultiplexer connection = ConnectionMultiplexer.Connect(redisConnection);
-            IDatabase cachedb = connection.GetDatabase();
-            for (int i = 0; i < 10; i++)
-            {
-                cachedb.StringIncrement(i.ToString());
-                Console.WriteLine("value=" + cachedb.StringGet(i.ToString()));
-    
-            }
-            
 
+            
             // Get the credentials
             // You can find instructions on how to get the token here:
             // http://msdn.microsoft.com/en-us/library/azure/dn790557.aspx
@@ -56,10 +48,10 @@ namespace CustomMonitoring
             // Get the events for an Azure Resource (e.g. Website) (as described by the Azure Resource Manager APIs here:http://msdn.microsoft.com/en-us/library/azure/dn790569.aspx)
             // A resource URI looks like the following string: 
             //"/subscriptions/########-####-####-####-############/resourceGroups/resourcegroupname1/providers/resourceprovider1/resourcename1"
-            string resourceUri = string.Format("subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.Cache/redis/{2}", subscriptionId,resourceGroupName, cacheName);
+            string resourceUri = string.Format("subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.Cache/redis/{2}", subscriptionId, resourceGroupName, cacheName);
 
             //Define a FilterString
-            string filterString = FilterString.Generate<ListEventsForResourceParameters>(eventData => (eventData.EventTimestamp >= startTime) && (eventData.EventTimestamp <= endTime) && (eventData.ResourceUri == resourceUri));
+            string filterString = FilterString.Generate<ListEventsForResourceParameters>(eventData => (eventData.EventTimestamp >= startTime) && (eventData.EventTimestamp <= endTime) && (eventData.ResourceId == resourceUri));
 
             //Get the events logs
             EventDataListResponse response = client.EventOperations.ListEvents(filterString, selectedProperties: null);
@@ -69,11 +61,11 @@ namespace CustomMonitoring
             Console.WriteLine("HTTP Status Code returned for the call:" + response.StatusCode);
 
             var response2 = client.MetricDefinitionOperations.GetMetricDefinitions(resourceUri, "");
-            var metrics = client.MetricOperations.GetMetrics(resourceUri, "startTime eq 2015-01-14T02:19:03.0712821Z and endTime eq 2015-01-14T03:19:03.0712821Z and timeGrain eq duration'PT5M'", response2.MetricDefinitionCollection.Value);
+            var metrics = client.MetricOperations.GetMetrics(resourceUri, "startTime eq " + startTime.ToString("yyyy-MM-ddThh:mm:ss") + "and endTime eq " + endTime.ToString("yyyy-MM-ddThh:mm:ss") + " and timeGrain eq duration'PT5M'", response2.MetricDefinitionCollection.Value);
             Console.WriteLine("Print out the metrics logs");
             foreach (var item in metrics.MetricCollection.Value)
             {
-                Console.WriteLine(item.Name.Value + "--" +item.MetricValues.Count);
+                Console.WriteLine(item.Name.Value + "--" + item.MetricValues.Count);
             }
 
         }
@@ -81,14 +73,16 @@ namespace CustomMonitoring
         {
             AuthenticationResult result = null;
 
-            var context = new AuthenticationContext(String.Format("https://login.windows.net/{0}", tenantId));
-
+            var url1 = "https://login.windows.net/"; // or "https://login.chinacloudapi.cn" for China
+            var url2 = "https://management.core.windows.net/"; // or "https://management.core.chinacloudapi.cn/" for China
+            var context = new AuthenticationContext(new Uri(new Uri(url1), tenantId).AbsoluteUri);
             var thread = new Thread(() =>
             {
+
                 result = context.AcquireToken(
-                  "https://management.core.windows.net/",
-                  clientId,
-                  new Uri(redirectUri));
+                  url2,
+                  "1950a258-227b-4e31-a9cf-717495945fc2", // clientId
+                  new Uri("urn:ietf:wg:oauth:2.0:oob"));
             });
 
             thread.SetApartmentState(ApartmentState.STA);
